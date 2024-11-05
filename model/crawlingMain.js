093a1395ba73279da2a5e09b0271c2345a69950b
sjml2002 <sjml2002@naver.com>
2024-11-05 23:57:57 +0900

    오늘 날짜 제출에 관한 크롤링은 되긴 됐는데, 백준 쪽에서 금지하니까 1시간에 한번 정도? 하는 걸로 트래픽에 영향 안가게 바꿔야할듯. 그리고 발표할 때도 이 이야기 해야할 것 같다.

--- model/crawlingMain.js
+++ model/crawlingMain.js
@@ -4,16 +4,19 @@ import * as crawlingStudent from "./crawlingStudent.js"
 import * as crawlingSubmit from "./crawlingSubmitTime.js"
 
 const cache_numofpeople = 0;
-const cache_lasttime = 0;
+const cache_lastdatetime = 0;
 let kwstudents = []
+let kwsubmitlist = []
 
-const getHtml = async(url) => {
+const getHtml = async(customheader, url) => {
     try {
-        const html  = await axios.get(url);
+        const html  = await axios.get(url, {
+            headers: customheader,
+        });
         const htmldata = cheerio.load(html.data)
         return (htmldata);
     } catch (error) {
-        console.error(error)
+        console.error(error) //debug
         return (-1);
     }
 }
@@ -23,7 +26,7 @@ const getHtml = async(url) => {
 //return: Array of kwStudentInfo
 const getkwStudentInfo = async() => {
     const url = "https://solved.ac/ranking/o/222"
-    const mainhtml = await getHtml(url)
+    const mainhtml = await getHtml("", url)
     if (mainhtml == -1) 
         return ("Error! html 데이터 추출 중 에러")
 
@@ -40,6 +43,10 @@ const getkwStudentInfo = async() => {
             return (ksiarray)
         }
     }
+    else { //그냥 이미 저장된 데이터 list return
+        console.log("kwstudents는 cache에 저장된거 return 했쪄") //debug
+        return (kwstudents)
+    }
 }
 
 //return: 제출 시간 순으로 정렬
@@ -47,21 +54,51 @@ const getSubmitOrderTime = async() => {
     const result_id = 4; //-1: 전체, 4: 맞았습니다
     const school_id = 222; //222: 광운대학교
     const url = `https://www.acmicpc.net/status?&result_id=${result_id}&school_id=${school_id}`
-    const mainhtml = await getHtml(url)
+    const header = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+    }
+
+    console.log(url) //debug
+
+    const mainhtml = await getHtml(header, url)
     if (mainhtml == -1)
         return ("Error! html 데이터 추출 중 에러")
 
-    let recentdate, recentTime = await crawlingSubmit.getRecentTime(mainhtml)
+    let recentdatetime = await crawlingSubmit.getRecentTime(mainhtml)
     
+    if (cache_lastdatetime < recentdatetime) {
+        //새롭게 업데이트 진행
+        const today = new Date();
+        today.setHours(0, 0, 0, 0); //오늘 자정으로 설정
+        let submitlist = await crawlingSubmit.getRecent_to_targettime_submitlist(mainhtml, today);
+        return (submitlist)
+    }
+    else { //그냥 이미 저장되어있던 데이터 list return
+        console.log("kwsublist는 cache에 저장된거 return 했쪄") //debug
+        return (kwsubmitlist)
+    }
 }
 
 
 ///////////////////////////////////////////////////////////
 
 const testmain = async() => {
-    const ksi = await getkwStudentInfo();
-    if (ksi.includes("Error!")) {
-        console.log(ksi); //debug
-    }
+    // const ksi = await getkwStudentInfo();
+    // if (ksi.includes("Error!")) {
+    //     console.log(ksi); //debug
+    // }
+    // else {
+    //     kwstudents = ksi;
+    //     console.log("테스트: ", kwstudents) //success debug
+    // }
+
+    // const sot = await getSubmitOrderTime();
+    // if (sot.includes("Error!")) {
+    //     console.log(sot)
+    // }
+    // else {
+    //     kwsubmitlist = sot;
+    //     console.log("테스트: ", kwsubmitlist)
+    // }
 }
 testmain();
--- model/crawlingSubmitTime.js
+++ model/crawlingSubmitTime.js
@@ -1,48 +1,76 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
 import kwStudentInfo from "./DTO/submitWithTime.js"
+import submitWithTime from "./DTO/submitWithTime.js";
 
-const getHtml = async(url) => {
+const getHtml = async(customheader, url) => {
     try {
-        const html  = await axios.get(url);
+        const html  = await axios.get(url, {
+            headers: customheader,
+        });
         const htmldata = cheerio.load(html.data)
         return (htmldata);
     } catch (error) {
-        console.error(error)
+        console.error(error) //debug
         return (-1);
     }
 }
 
-//YYYY년 MM월 DD일 HH:MM:SS (MM월은 M월, DD일은 D일 일수도 있다.)
-//YYYY년 MM월 DD일을 日수로 (Date)
-//HH:MM:SS 을 초 단위로 (Time)
-//return: date, time
+//YYYY-MM-DD HH:MM:SS (MM월은 M월, DD일은 D일 일수도 있다.)
+//return: datetime (class Date)
 function stringTo_DateTime(timestr) {
-    const splittimestr = timestr.split(' ')
-
-    const year = splittimestr[0].slice(0, 4);
-    const month = splittimestr[1].slice(0, splittimestr[1].length-1);
-    const day = splittimestr[2].slice(0, splittimestr[2].length-1);
-    const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    let date = (year - 1) * 365; // 전년도까지의 총 일수
-    for(let i=0; i<month-1; i++) {
-        date += daysInMonth[i];
-    }
-    date += day;
-    
-    const hour = splittimestr[3][0] + splittimestr[3][1]
-    const minute = splittimestr[3][3] + splittimestr[3][4]
-    const second = splittimestr[3][6] + splittimestr[3][7]
-    const time = hour*3600 + minute*60 + second;
-
-    return (date, time)
+    const datetime = new Date(timestr);
+    return (datetime);
 }
 
 //현재 광운대학교 학생이 푼 문제 중 가장 최상위 문제의 시간
 export const getRecentTime = async($) => {
+    const recenttimestr = $("tbody").find('tr').find("td").eq(8).children("a").attr("title")
+    const datetime = stringTo_DateTime(recenttimestr)
+    return (datetime);
+}
+
+
+//return: recent(최근시각) ~ targettime 까지 광운대학교 학생들의 제출 기록을 list로 반환
+export const getRecent_to_targettime_submitlist = async($, targetdatetime) => {
+    let submitlist = [] //class: submitWithTime
+
+    while(1) {
+        let stopflag = 0; //1이면 while문 break
+        let trlist = $("tbody").find('tr');
+
+        trlist.each((idx, tr) => {
+            const tdlist = $(tr).find("td")
+            
+            const id = $(tdlist).eq(1).children("a").text()
+            const pid = $(tdlist).eq(2).children("a").text()
+            const t = $(tdlist).eq(8).children("a").attr("title")
     
+            //탐색한 t가 targettime까지 인지
+            let curdatetime = stringTo_DateTime(t)
+            //curdate는 점점 감소중
 
+            console.log(targetdatetime, curdatetime); //debug
 
+            if (targetdatetime <= curdatetime)
+                submitlist.push(new submitWithTime(id, pid, t))
+            else { //curdate & curtime이 target보다 작아짐. 탐색 중지
+                stopflag = 1;
+                return false //break
+            }
+        });
 
-    //return (date, time)
+        if (stopflag)
+            break
+        else { //다음페이지로 이동
+            const nextpageurl = "https://www.acmicpc.net" + $("a#next_page").attr("href")
+            const header = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+            };
+
+            $ = await getHtml(header, nextpageurl)
+        }
+    }    
+
+    return (submitlist)
 }
