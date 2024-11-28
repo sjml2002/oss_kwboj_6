3b9a0183e39ab187ddf68b00231b6cdf2c513f6f
jehee <jhlee001010@naver.com>
2024-11-28 18:52:30 +0900

    잔디밭까지 합치기 완료

--- model/crawlingMain.js
+++ model/crawlingMain.js
@@ -1,6 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
 import * as cheerio from "cheerio";
-//import * as fs from 'fs';
 import * as crawlingStudent from "./crawlingStudent.js";
 import * as crawlingSubmit from "./crawlingSubmitTime.js";
 import * as crawlingUniRank from "./crawlingUniversityRanking.js";
@@ -8,14 +7,11 @@ import * as crawlingUniRank from "./crawlingUniversityRanking.js";
 let cache_numofpeople = 0;
 let cache_lastdatetime = 0;
 let cache_lastupdate = new Date("2000-01-01 00:00:00"); //마지막으로 업데이트 한 시간
-let cache_lasttodayproblem = new Date("2000-01-01 00:00:00"); //today problem 마지막 업데이트
 
 //view에 쓸 데이터
-let data_kwstudents = [];
-let data_kwsubmitlist = [];
-let data_unirank = new Map();
-let data_totalProblems = new Set();
-let data_todaysProblem = [];
+let data_kwstudents = []
+let data_kwsubmitlist = []
+let data_unirank = []
 
 const getHtml = async(customheader, url) => {
     try {
@@ -64,12 +60,8 @@ export const getkwStudentInfo = async() => {
     }
 }
 
-/**
- * targetTime ~ 호출한 시각까지 모두 구해옴
- * @param {Date} targetTime 
- * @returns {list} data_kwsubmitlist
- */
-export const getSubmitOrderTime = async(targetTime) => {
+//return: 시간 순으로 정렬된 submitWithTime
+export const getSubmitOrderTime = async() => {
     const result_id = 4; //-1: 전체, 4: 맞았습니다
     const school_id = 222; //222: 광운대학교
     const url = `https://www.acmicpc.net/status?&result_id=${result_id}&school_id=${school_id}`
@@ -84,11 +76,14 @@ export const getSubmitOrderTime = async(targetTime) => {
         return ("Error! html 데이터 추출 중 에러")
 
     let recentdatetime = await crawlingSubmit.getRecentTime(mainhtml)
-    
+
     if (cache_lastdatetime < recentdatetime) {
-        let newsubmitlist = await crawlingSubmit.getRecent_to_targettime_submitlist(mainhtml, cache_lastdatetime);
-        data_kwsubmitlist.push(newsubmitlist);
         cache_lastdatetime = recentdatetime; //cache update
+        //새롭게 업데이트 진행
+        const yearago = new Date();
+        yearago.setFullYear(yearago.getFullYear()-1);
+        yearago.setHours(0, 0, 0, 0); //오늘 자정으로 설정
+        data_kwsubmitlist = await crawlingSubmit.getRecent_to_targettime_submitlist(mainhtml, yearago);
         return (data_kwsubmitlist)
     }
     else { //그냥 이미 저장되어있던 데이터 list return
@@ -102,7 +97,7 @@ export const getUniversityRanking = async() => {
     const today = new Date();
     let diffDate = today.getTime() - cache_lastupdate.getTime();
     diffDate = Math.abs(diffDate / (1000*60*60*24)); //밀리세컨*초*분*시 = 일
-    if (diffDate < 1 && data_unirank.size != 0)
+    if (diffDate < 1 && !data_unirank.empty())
         return (data_unirank);
     //업데이트 어차피 할거니까 cache를 오늘 자정으로 설정
     cache_lastupdate = today;
@@ -120,49 +115,8 @@ export const getUniversityRanking = async() => {
     return (data_unirank);
 }
 
-//광운대학생이 푼 총 문제
-// export const getTotalProblem = async() => {
-//     data_kwstudents = await getkwStudentInfo();
-//     const studentsID = data_kwstudents.map((student) => student._ID);
-
-//     data_totalProblems = await crawlingStudent.updateTotalProblem(studentsID);
-//     console.log("data_totalProblems: ", data_totalProblems); //debug
-//     const jsondata = JSON.stringify([...data_totalProblems]);
-//     fs.writeFileSync("test.json", jsondata);
-
-//     console.log("여기는? ", data_totalProblems); //debug
-//     return (data_totalProblems);
-// }
-
-//오늘의 추천 문제 3문제
-export const getTodaysProblem = () =>  {
-    //오늘 이미 업데이트 되었다면 딱히 추천하지 않기
-    const today = new Date();
-    let diffDate = today.getTime() - cache_lasttodayproblem.getTime();
-    diffDate = Math.abs(diffDate / (1000*60*60*24)); //밀리세컨*초*분*시 = 일
-    if (diffDate < 1 && data_todaysProblem.length != 0)
-        return (data_todaysProblem);
-    cache_lasttodayproblem = today;
-
-    console.log("오늘의 문제 새로 업데이트"); //debug
-    const bojmin = 1000; //제일 처음 문제번호
-    const bojmax = 32827; //제일 마지막 문제번호
-    const jsondata = fs.readFileSync(`json/totalProblem.json`);
-    const parsedata = JSON.parse(jsondata); //광운대학생이 푼 문제 array
-
-    data_todaysProblem = []; //초기화
-    while (data_todaysProblem.length < 5) {
-        const randomval = Math.floor(Math.random() * (bojmax - bojmin + 1)) + bojmin;
-        if (parsedata.includes(randomval.toString()))
-            continue;
-        else
-        data_todaysProblem.push(randomval);
-    }
-    return (data_todaysProblem);
-}
-
 ///////////////////////////////////////////////////////////
-//debug
+
 // const testmain = async() => {
 //     // const ksi = await getkwStudentInfo();
 //     // if (ksi.includes("Error!")) {
@@ -190,15 +144,212 @@ export const getTodaysProblem = () =>  {
 //     //     data_unirank = unirank;
 //     //     console.log("테스트: ", data_unirank)
 //     // }
-
-//     // data_totalProblems = await getTotalProblem();
-//     // console.log("테스트: ", data_totalProblems); //deubg
-//     // data_totalProblems = await getTotalProblem();
-//     // console.log("캐시 테스트: ", data_totalProblems); //debug
-
-//     // data_todaysProblem = getTodaysProblem();
-//     // console.log(data_todaysProblem);
-//     // data_todaysProblem = getTodaysProblem();
-//     // console.log(data_todaysProblem);
 // }
 // testmain();
+
+//////////////////////////////////////////////////////////////////////////// 이전코드 11-28 준혁코드 받아옴
+
+// import axios from 'axios';
+// import * as cheerio from "cheerio";
+// //import * as fs from 'fs';
+// import * as crawlingStudent from "./crawlingStudent.js";
+// import * as crawlingSubmit from "./crawlingSubmitTime.js";
+// import * as crawlingUniRank from "./crawlingUniversityRanking.js";
+
+// let cache_numofpeople = 0;
+// let cache_lastdatetime = 0;
+// let cache_lastupdate = new Date("2000-01-01 00:00:00"); //마지막으로 업데이트 한 시간
+// let cache_lasttodayproblem = new Date("2000-01-01 00:00:00"); //today problem 마지막 업데이트
+
+// //view에 쓸 데이터
+// let data_kwstudents = [];
+// let data_kwsubmitlist = [];
+// let data_unirank = new Map();
+// let data_totalProblems = new Set();
+// let data_todaysProblem = [];
+
+// const getHtml = async(customheader, url) => {
+//     try {
+//         const html  = await axios.get(url, {
+//             headers: customheader,
+//         });
+//         const htmldata = cheerio.load(html.data)
+//         return (htmldata);
+//     } catch (error) {
+//         console.error(error) //debug
+//         return (-1);
+//     }
+// }
+
+// /////////////////////////////////////////////////////////////
+
+// //return: Array of kwStudentInfo
+// export const getkwStudentInfo = async() => {
+//     const url = "https://solved.ac/ranking/o/222"
+//     const header = {
+//         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+//     }
+//     const mainhtml = await getHtml(header, url)
+//     if (mainhtml == -1) 
+//         return ("Error! html 데이터 추출 중 에러")
+
+//     const numofpeople = await crawlingStudent.curPeopleCnt(mainhtml);
+//     console.log(numofpeople, cache_numofpeople); //debug
+
+//     if (numofpeople > cache_numofpeople) { //kwStudentInfo 배열 업데이트
+//         cache_numofpeople = numofpeople; //cache update
+//         console.log("kwStudentInfo 업데이트 해야됨") //debug
+//         const ksiarray = await crawlingStudent.updateKwStudentInfo(mainhtml);
+
+//         //이거 그냥 return (ksiarray) 한 다음에 view(front-end) 단에서 처리해도 될듯
+//         if (ksiarray.empty)
+//             return ("Error! kwStudentInfo 업데이트 중 에러")
+//         else {   //최종 return
+//             data_kwstudents = ksiarray
+//             return (data_kwstudents)
+//         }
+//     }
+//     else { //그냥 이미 저장된 데이터 list return
+//         console.log("kwstudents는 cache에 저장된거 return 했쪄") //debug
+//         return (data_kwstudents)
+//     }
+// }
+
+// /**
+//  * targetTime ~ 호출한 시각까지 모두 구해옴
+//  * @param {Date} targetTime 
+//  * @returns {list} data_kwsubmitlist
+//  */
+// export const getSubmitOrderTime = async(targetTime) => { // 여기가 다름??????????????????????????????????????????????????????
+//     const result_id = 4; //-1: 전체, 4: 맞았습니다
+//     const school_id = 222; //222: 광운대학교
+//     const url = `https://www.acmicpc.net/status?&result_id=${result_id}&school_id=${school_id}`
+//     const header = {
+//         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+//     }
+
+//     console.log(url) //debug
+
+//     const mainhtml = await getHtml(header, url)
+//     if (mainhtml == -1)
+//         return ("Error! html 데이터 추출 중 에러")
+
+//     let recentdatetime = await crawlingSubmit.getRecentTime(mainhtml)
+    
+//     if (cache_lastdatetime < recentdatetime) {
+//         let newsubmitlist = await crawlingSubmit.getRecent_to_targettime_submitlist(mainhtml, cache_lastdatetime);
+//         data_kwsubmitlist.push(newsubmitlist);
+//         cache_lastdatetime = recentdatetime; //cache update
+//         return (data_kwsubmitlist)
+//     }
+//     else { //그냥 이미 저장되어있던 데이터 list return
+//         console.log("kwsublist는 cache에 저장된거 return 했쪄") //debug
+//         return (data_kwsubmitlist)
+//     }
+// }
+
+// export const getUniversityRanking = async() => {
+//     //하루에 한번씩만 업데이트 하도록 설정
+//     const today = new Date();
+//     let diffDate = today.getTime() - cache_lastupdate.getTime();
+//     diffDate = Math.abs(diffDate / (1000*60*60*24)); //밀리세컨*초*분*시 = 일
+//     if (diffDate < 1 && data_unirank.size != 0)
+//         return (data_unirank);
+//     //업데이트 어차피 할거니까 cache를 오늘 자정으로 설정
+//     cache_lastupdate = today;
+
+//     const url = "https://www.acmicpc.net/ranklist/university/1";
+//     const header = {
+//         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+//     }
+
+//     const mainhtml = await getHtml(header, url);
+//     if (mainhtml == -1)
+//         return ("Error! html 데이터 추출 중 에러")
+
+//     data_unirank = await crawlingUniRank.unirank100(mainhtml)
+//     return (data_unirank);
+// }
+
+// //광운대학생이 푼 총 문제
+// // export const getTotalProblem = async() => {
+// //     data_kwstudents = await getkwStudentInfo();
+// //     const studentsID = data_kwstudents.map((student) => student._ID);
+
+// //     data_totalProblems = await crawlingStudent.updateTotalProblem(studentsID);
+// //     console.log("data_totalProblems: ", data_totalProblems); //debug
+// //     const jsondata = JSON.stringify([...data_totalProblems]);
+// //     fs.writeFileSync("test.json", jsondata);
+
+// //     console.log("여기는? ", data_totalProblems); //debug
+// //     return (data_totalProblems);
+// // }
+
+// //오늘의 추천 문제 3문제
+// export const getTodaysProblem = () =>  {
+//     //오늘 이미 업데이트 되었다면 딱히 추천하지 않기
+//     const today = new Date();
+//     let diffDate = today.getTime() - cache_lasttodayproblem.getTime();
+//     diffDate = Math.abs(diffDate / (1000*60*60*24)); //밀리세컨*초*분*시 = 일
+//     if (diffDate < 1 && data_todaysProblem.length != 0)
+//         return (data_todaysProblem);
+//     cache_lasttodayproblem = today;
+
+//     console.log("오늘의 문제 새로 업데이트"); //debug
+//     const bojmin = 1000; //제일 처음 문제번호
+//     const bojmax = 32827; //제일 마지막 문제번호
+//     const jsondata = fs.readFileSync(`json/totalProblem.json`);
+//     const parsedata = JSON.parse(jsondata); //광운대학생이 푼 문제 array
+
+//     data_todaysProblem = []; //초기화
+//     while (data_todaysProblem.length < 5) {
+//         const randomval = Math.floor(Math.random() * (bojmax - bojmin + 1)) + bojmin;
+//         if (parsedata.includes(randomval.toString()))
+//             continue;
+//         else
+//         data_todaysProblem.push(randomval);
+//     }
+//     return (data_todaysProblem);
+// }
+
+// ///////////////////////////////////////////////////////////
+// //debug
+// // const testmain = async() => {
+// //     // const ksi = await getkwStudentInfo();
+// //     // if (ksi.includes("Error!")) {
+// //     //     console.log(ksi); //debug
+// //     // }
+// //     // else {
+// //     //     data_kwstudents = ksi;
+// //     //     console.log("테스트: ", data_kwstudents) //success debug
+// //     // }
+
+// //     // const sot = await getSubmitOrderTime();
+// //     // if (sot.includes("Error!")) {
+// //     //     console.log(sot)
+// //     // }
+// //     // else {
+// //     //     data_kwsubmitlist = sot;
+// //     //     console.log("테스트: ", data_kwsubmitlist)
+// //     // }
+
+// //     // const unirank = await getUniversityRanking();
+// //     // if (unirank.includes("Error!")) {
+// //     //     console.log(unirank)
+// //     // }
+// //     // else {
+// //     //     data_unirank = unirank;
+// //     //     console.log("테스트: ", data_unirank)
+// //     // }
+
+// //     // data_totalProblems = await getTotalProblem();
+// //     // console.log("테스트: ", data_totalProblems); //deubg
+// //     // data_totalProblems = await getTotalProblem();
+// //     // console.log("캐시 테스트: ", data_totalProblems); //debug
+
+// //     // data_todaysProblem = getTodaysProblem();
+// //     // console.log(data_todaysProblem);
+// //     // data_todaysProblem = getTodaysProblem();
+// //     // console.log(data_todaysProblem);
+// // }
+// // testmain();
